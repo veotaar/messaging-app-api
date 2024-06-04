1a4a3159769150eb3f95190cf25bb9fc1372d7a8
Yalçıncan Ulus <41164799+veotaar@users.noreply.github.com>
2024-06-04 22:47:29 +0300

    add the ability to issue jwts

--- src/utils/connect.ts
+++ src/utils/connect.ts
@@ -2,12 +2,17 @@ import mongoose from 'mongoose';
 import config from 'config';
 import log from './logger';
 
+
+const writeConcern = new mongoose.mongo.WriteConcern('majority');
+
 const connect = async () => {
   const dbString = config.get<string>('dbString');
 
   try {
     await mongoose.connect(dbString, {
       dbName: 'messaging-api',
+      retryWrites: true,
+      writeConcern: writeConcern,
     });
     log.info('Connected to DB');
   } catch (error) {
---
+++ src/utils/jwt.utils.ts
@@ -0,0 +1,34 @@
+import jwt from 'jsonwebtoken';
+import config from 'config';
+import { User } from '../models/user.model';
+import { DocumentType } from '@typegoose/typegoose';
+
+const privateKey = Buffer.from(
+  config.get<string>('privateKey'),
+  'base64'
+).toString('ascii');
+
+const publicKey = Buffer.from(
+  config.get<string>('publicKey'),
+  'base64'
+).toString('ascii');
+
+export const issueJwt = (user: DocumentType<User>) => {
+  const id = user._id.toString();
+  const expiresIn = '8h';
+
+  const payload = {
+    sub: id,
+    iat: Math.floor(Date.now() / 1000)
+  };
+
+  const token = jwt.sign(payload, privateKey, {
+    expiresIn,
+    algorithm: 'RS256'
+  });
+
+  return {
+    token: `Bearer ${token}`,
+    expires: payload.iat + (8 * 60 * 60)
+  };
+};
