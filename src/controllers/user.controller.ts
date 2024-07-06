7cbdbd327fbd969eaba78f95b76c51b293234d13
Yalçıncan Ulus <41164799+veotaar@users.noreply.github.com>
2024-07-06 23:54:01 +0300

    issue jwt after creating a user

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -2,11 +2,16 @@ import { Request, Response } from "express";
 import log from "../utils/logger";
 import { createUser } from "../services/user.service";
 import { CreateUserInput } from "../schema/user.schema";
+import { issueJwt } from "../utils/jwt.utils";
 
 export const createUserHandler = async (req: Request<{}, {}, CreateUserInput['body']>, res: Response) => {
   try {
     const user = await createUser(req.body);
-    return res.json(user);
+    const jwt = issueJwt(user);
+    return res.json({
+      user,
+      jwt
+    });
   } catch (e) {
     log.error(e);
     return res.sendStatus(409);
